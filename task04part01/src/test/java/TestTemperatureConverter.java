24140645acde6668dffe038fcbda0937c082e4c2
sexfoil <polyakov_slava@ukr.net>
2018-10-24 19:35:29 +0300

    task04 part01 - commit changes

--- task04part01/src/test/java/TestTemperatureConverter.java
+++ task04part01/src/test/java/TestTemperatureConverter.java
@@ -1,40 +1,43 @@
 import static org.junit.Assert.*;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TestTemperatureConverter {
-    TemperatureConverter converter;
 
-    @Before
-    public void initialize() {
-        converter = new TemperatureConverter();
+    @Parameterized.Parameter
+    public double temperatureC;
+
+    @Parameterized.Parameter(1)
+    public double temperatureK;
+
+    @Parameterized.Parameter(2)
+    public double temperatureF;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getTestsData() {
+        return Arrays.asList(new Object[][] {
+                {0.0, 273.2, 32.0},
+                {-100.0, 173.2, -148.0},
+                {-273.2, 0.0, -459.7},
+                {-17.77, 255.38, 0.0}
+
+        });
     }
 
     @Test
     public void testTemperatureConverter() {
-        assertEquals(-4.0, converter.convertCtoF(-20), 0.0001);
-        assertEquals(32.0, converter.convertCtoF(0), 0.0001);
-        assertEquals(68.0, converter.convertCtoF(20), 0.0001);
-
-        assertEquals(-45.55555, converter.convertFtoC(-50), 0.0001);
-        assertEquals(-17.77777, converter.convertFtoC(0), 0.0001);
-        assertEquals(10.0, converter.convertFtoC(50), 0.0001);
-
-        assertEquals(-573.2, converter.convertKtoC(-300), 0.0001);
-        assertEquals(-273.2, converter.convertKtoC(0), 0.0001);
-        assertEquals(26.8, converter.convertKtoC(300), 0.0001);
-
-        assertEquals(-226.8, converter.convertCtoK(-500), 0.0001);
-        assertEquals(273.2, converter.convertCtoK(0), 0.0001);
-        assertEquals(773.2, converter.convertCtoK(500), 0.0001);
-
-
-        assertEquals(-22.38888, converter.convertFtoK(-500), 0.0001);
-        assertEquals(255.38888, converter.convertFtoK(0), 0.0001);
-        assertEquals(533.16666, converter.convertFtoK(500), 0.0001);
-
-        assertEquals(-999.7, converter.convertKtoF(-300), 0.0001);
-        assertEquals(-459.7, converter.convertKtoF(0), 0.0001);
-        assertEquals(80.3, converter.convertKtoF(300), 0.0001);
+        TemperatureConverter converter = new TemperatureConverter();
+        assertEquals(temperatureF, converter.convertCtoF(temperatureC), 0.1);
+        assertEquals(temperatureC, converter.convertFtoC(temperatureF), 0.1);
+        assertEquals(temperatureK, converter.convertCtoK(temperatureC), 0.1);
+        assertEquals(temperatureC, converter.convertKtoC(temperatureK), 0.1);
+        assertEquals(temperatureF, converter.convertKtoF(temperatureK), 0.1);
+        assertEquals(temperatureK, converter.convertFtoK(temperatureF), 0.1);
     }
 }
